5238b6324f5be74e43af6c43e3ce51e810ce1013
Tommaso Giacometti <tommaso.giacometti4@studio.unibo.it>
2023-06-03 17:16:33 +0200

    Building FFNN, the train works but result are wrong :(

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torch
 from torch_geometric.utils import train_test_split_edges
 import models
 import plots
+import numpy as np
 
 
 
@@ -22,7 +23,8 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = current_dir + '/data'
 
 #Download data (if not already present in the specific folder)
-dataset = Planetoid(root=data_dir, name='Cora')
+norm = pyg.transforms.NormalizeFeatures()
+dataset = Planetoid(root=data_dir, name='Cora', transform=norm)
 data = dataset[0].to(device)
 
 torch.manual_seed(0)
@@ -39,32 +41,83 @@ test_neg = data.test_neg_edge_index
 #%%
 #Creation of the model
 in_channels = data.x.shape[1]
-hid_dim = 32
-emb_dim = 16
+hid_dim = 100
+emb_dim = 50
 lr = 1e-2
-model = models.VGAE(in_channels, hid_dim, emb_dim)
+autoencoder = models.VGAE(in_channels, hid_dim, emb_dim)
 
 
 #%%
 #Training
-epochs = 500
-norm = 1/data.x.shape[0]
-
-lossi = model.train_cycle(data.x, train_pos, test_pos, test_neg)
+lossi = autoencoder.train_cycle(data.x, train_pos, test_pos, test_neg)
 
     
 #%%
 #Plots
 
 plots.plot_loss(lossi)
-plots.plot_train_distribution(model, data.x, train_pos)
-plots.plot_test_distribution(model, data.x, train_pos, test_pos, test_neg)
+plots.plot_train_distribution(autoencoder, data.x, train_pos)
+plots.plot_test_distribution(autoencoder, data.x, train_pos, test_pos, test_neg)
+
+
+#%%
+#Data processing for the FFNN
+embedding = autoencoder(data.x, train_pos)[0].detach() #[0] -> To get only z and not logvar
+
+train_emb = models.get_ffnn_input(embedding, train_pos)
+test_emb_pos = models.get_ffnn_input(embedding, test_pos)
+test_emb_neg = models.get_ffnn_input(embedding, test_neg)
+
+train_emb_neg = test_emb_neg
 
 
 
+#%%
+#FFNN
+
+ffnn = models.FFNN(emb_dim*2).to(device)
+
+epochs = 200
+batch_size = 128
+optim = torch.optim.Adam(ffnn.parameters(),lr = 1e-3)
+ffnn.train()
+loss_fn = torch.nn.CrossEntropyLoss()
+index = np.random.randint(0, train_emb.shape[0]-batch_size, epochs)
+
+one = torch.ones(batch_size, dtype=torch.long).to(device)
+zero = torch.zeros(batch_size, dtype=torch.long).to(device)
+
+lossi = []
+
+for i, ind in enumerate(index):
+    optim.zero_grad()
+
+    # neg = pyg.utils.negative_sampling(train_pos, data.x.shape[0], batch_size)
+    # neg = models.get_ffnn_input(embedding, neg)
+
+    out_pos = ffnn(train_emb[ind:ind+batch_size])
+    #out_neg = ffnn(neg)
+    loss = loss_fn(out_pos, one) #+ loss_fn(out_neg, zero)
+    loss.backward()
+    optim.step()
+    lossi.append(loss.item())
+
+print(loss.item())
+plots.plot_loss(lossi, 100)
+
+with torch.no_grad():
+    one = torch.ones(test_emb_pos.shape[0], dtype=torch.long).to(device)
+    zero = torch.zeros(test_emb_neg.shape[0], dtype=torch.long).to(device)
+    out_pos = ffnn(test_emb_pos)
+    out_neg = ffnn(test_emb_neg)
+    loss = loss_fn(out_pos, one) + loss_fn(out_neg, zero)
+    print(loss.item())
 
 
+#%%
+link = test_pos.T[0:2]
+inp = models.get_ffnn_input(embedding, link)
 
-
+torch.nn.functional.softmax(ffnn(inp[1]), dim = 0)
 
 
--- models.py
+++ models.py
@@ -7,8 +7,10 @@ Created on Thu Jun  1 20:21:48 2023
 """
 import torch
 import torch_geometric as pyg
+from torch import nn
 
-class GCNEncoder(torch.nn.Module):
+
+class GCNEncoder(nn.Module):
     
     def __init__(self, in_channels : int, hid_dim : int, emb_dim : int):
         super().__init__()
@@ -30,7 +32,9 @@ class VGAE(pyg.nn.VGAE):
         super().__init__(encoder=GCNEncoder(in_channels, hid_dim, emb_dim))
     
         
-    def train_step(self, optimizer, x, edges):
+    def train_step(self, x, edges, optimizer = None):
+        if optimizer is None:
+            optimizer = torch.optim.Adam(self.parameters(),lr=1e-2)
         norm = 1/x.shape[0]
         self.train()
         optimizer.zero_grad()
@@ -49,12 +53,12 @@ class VGAE(pyg.nn.VGAE):
             print(f'AUC: {auc:.4f} | AP: {ap:.4f}')
             
     
-    def train_cycle(self, x, train_pos, test_pos, test_neg, epochs = 500, optimizer = None):
+    def train_cycle(self, x, train_pos, test_pos, test_neg, epochs = 1000, optimizer = None):
         if optimizer is None:
             optimizer = torch.optim.Adam(self.parameters(),lr=1e-2)
         lossi = []
         for i in range(epochs):
-            lossi.append(self.train_step(optimizer, x, train_pos))
+            lossi.append(self.train_step(x, train_pos, optimizer))
             if i%(epochs/20) == 0:
                 print(f'{i/epochs*100:.2f}% | loss = {lossi[i]:.4f} -> ', end = '')
                 self.test_step(x, train_pos, test_pos, test_neg)
@@ -64,8 +68,38 @@ class VGAE(pyg.nn.VGAE):
         return lossi
         
         
+
+class  FFNN(nn.Module):
+    
+    def __init__(self, inputs, out = 2):
+        super().__init__()
+        self.seq = nn.Sequential(nn.Linear(inputs, 20), nn.ReLU(),
+                                 nn.Linear(20, 10), nn.ReLU(),
+                                 nn.Linear(10, out))
         
         
+    def forward(self,x):
+        logits = self.seq(x)
+        return logits
+        
+    
+def get_ffnn_input(embedding, links):
+    x = torch.hstack((embedding[0,:],embedding[1,:]))
+    for i in links.T:
+        row = torch.hstack((embedding[int(i[0]),:], embedding[int(i[1]),:]))
+        x = torch.vstack((x,row))
+    return x[1:].requires_grad_(True) #To avoid the first row used to define the dimensions
+    
+
+
+
+
+
+
+
+
+
+
         
         
         
